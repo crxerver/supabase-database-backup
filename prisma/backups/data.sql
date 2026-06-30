ee78926f4c7a28ed542107ba65dfcfa5c3502562
crxerver <crxerver@users.noreply.github.com>
2026-06-30 02:56:24 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict CcpOLD9iRcExqYilrDO2oQl2qCxMQU9zZiBP8strD4uc27k9pdXE3U5bZ3mZYOr
+-- \restrict MfxqXKTqp7JLGiyGhuArZtpQHrwk9iGPbUJ6eP38b9tZf6eCKd2qB12LBv9klpU
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -33,7 +33,7 @@ COPY "auth"."audit_log_entries" ("instance_id", "id", "payload", "created_at", "
 -- Data for Name: custom_oauth_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at") FROM stdin;
+COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at", "custom_claims_allowlist") FROM stdin;
 \.
 
 
@@ -1318,6 +1318,6 @@ SELECT pg_catalog.setval('"public"."user_profiles_id_seq"', 5, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict CcpOLD9iRcExqYilrDO2oQl2qCxMQU9zZiBP8strD4uc27k9pdXE3U5bZ3mZYOr
+-- \unrestrict MfxqXKTqp7JLGiyGhuArZtpQHrwk9iGPbUJ6eP38b9tZf6eCKd2qB12LBv9klpU
 
 RESET ALL;
